3a60481ff7509a35e6ebeed75197a4486f3077cf
teshi24 <nadja.stadi@gmail.com>
2022-04-21 18:20:59 +0200

    SW08 A1, 1. Aufgabe

---
+++ src/main/java/ch/hslu/ad/sw08/Triangle.java
@@ -0,0 +1,9 @@
+package ch.hslu.ad.sw08;
+
+public class Triangle implements Comparable<Triangle>{
+  @Override
+  public int compareTo(final Triangle o) {
+    // todo: implement functionality
+    throw new UnsupportedOperationException("functionality not yet implemented");
+  }
+}
---
+++ src/main/java/ch/hslu/ad/sw08/package-info.java
@@ -0,0 +1,42 @@
+/**
+ * 1 Grundlegende Zusammenhänge
+ * 1.3
+ * a) Triangle Klasse - Dreieck-Objekte, definiert durch 3 Eckpunkte (x/y)-Koordinaten
+ *    Mögliche Totale Ordnungen:
+ *    - Fläche absteigend, aufsteigend (kann mit den Eckpunkten berechnet werden)
+ *    - Punkte - Entfernung vom Ursprung des Koordinatensystem
+ *    - evtl. Tiefstes Dreieck bis Höhestes Dreieck / Dreiecke von Links nach Rechts im Koordinatensystem
+ *    - Vorgeschlagene natürliche Ordnung: sortieren nach Fläche von Kleinstem zum Grössten
+ *      Note: dann muss in der Dok einfach erwähnt werden, dass die natürliche Ordnung nicht zwingend equals entspricht!
+ * b) Triangle muss {@link java.lang.Comparable<Triangle>} implementieren und damit compareTo
+ *    Parameter:    Triangle triangle - ein Triangle Objekt (sichergestellt durch Typing beim importieren
+ *    Rückgabewert: int
+ *                  -> negative integer = this object is less than given object
+ *                  -> zero = this object is equal to given object
+ *                  -> positive integer = this object is greater than given object
+ *    Expectations: - signum(x.compareTo(y)) == -signum(y.compareTo(x)) is always true
+ *                  - when x.compareTo(y) throws an Exception, y.compareTo(x) throws an exception as well
+ *                  - (x.compareTo(y) > 0 && y.compareTo(z) > 0) implies that x.compareTo(z) > 0.
+ *                  - x.compareTo(y)==0 implies that signum(x.compareTo(z)) == signum(y.compareTo(z)), for all z.
+ *                  - strongly recommended but not strictly required that (x.compareTo(y)==0) == (x.equals(y))
+ *                    - if not, it must be indicated, e.g. "Note: this class has a natural ordering that is inconsistent with equals."
+ *    Exceptions: - NullPointerException – if the specified object is null
+ *                - ClassCastException – if the specified object's type prevents it from being compared to this object.
+ * c) neben equals() muss hashCode() überschrieben werden (beides Methoden von {@link java.lang.Object}
+ * d) für die spezielle Ordnung muss {@link java.util.Comparator<Triangle>} implementiert werden
+ *    -> zur Implementation würde ich eine statische Variable machen mit einem lambda-Comparator
+ *    @see <a href="https://www.baeldung.com/java-8-sort-lambda#sort-lambda">Java 8 sort lambda</a>
+ * e) von {@link java.util.Arrays} würden sich folgende Methoden für das Sortieren eignen:
+ *    - {@link java.util.Arrays#sort(java.lang.Object[])}
+ *      -> Sorts the specified array of objects into ascending order, according to the natural ordering of its elements.
+ *    - {@link java.util.Arrays#sort(java.lang.Object[], int, int)} )}
+ *      -> Sorts the specified range of the specified array of objects into ascending order, according to the natural ordering of its elements.
+ *    - {@link java.util.Arrays#sort(java.lang.Object[], java.util.Comparator)}
+ *      -> Sorts the specified array of objects according to the order induced by the specified comparator.
+ *    - {@link java.util.Arrays#sort(java.lang.Object[], int, int, java.util.Comparator)}
+ *      -> Sorts the specified range of the specified array of objects according to the order induced by the specified comparator.
+ *    -> alle sind stabile, iterative mergesort  s
+ *
+ *
+ */
+package ch.hslu.ad.sw08;
